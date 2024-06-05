ad76db809acbc2cacb392cee3b0b19735b37fb08
shezza <96924530+swampcreaturee@users.noreply.github.com>
2024-06-04 22:23:17 -0500

    silver spoon and haikyu movie

---
+++ src/content/reviews2024/haikyu-dumpster-battle.md
@@ -0,0 +1,17 @@
+---
+title: HAIKYU!! The Dumpster Battle
+type: movie
+year: 2024
+shortened: haikyu-dumpster-battle
+feel: loved
+pubdate: "06/04/2024"
+---
+
+I was excited for the dumpster battle to be animated and I think they did a great job with it. The series was always headed towards a rematch between Nekoma and Karasuno and getting to see that game on a movie screen gave it the elevated importance it deserved. The story was well paced and the flashbacks provided a nice refresher to the closeness of these two teams and the competitive spirit on display here. This match was about two teams with a healthy amount of competitive spirit with each other, but also a lot of love and camaraderie. Haikyu always excels in capturing that part of team sports, especially high school sports. It stresses me out so much when Kenma uses his big beautiful brain to trap Hinata, but man, it's so good.
+
+I loved the POV animation moments! It added such a dynamic perspective to the game and really immersed you into it in a way the manga didn't (and couldn't). It's a shame that some of the manga material will end up left out of the series by switching to a movie format, but I think it will be a big help to the story pacing as the manga gets translated to anime.
+
+## Misc notes
+- It brought me so much joy to see my boy Kageyama again <3 
+- I like making two movies with the rest of the material, but I'm very curious what they do with the other manga material
+- Other than the POV moments, nothing about the direction popped out at me? Idk maybe I'm dense lol
--- src/utils/txfepisode.js
+++ src/utils/txfepisode.js
@@ -1,37 +1,37 @@
-import myjson from "./txfepisode.json" assert { type: "json" };
-console.log(myjson);
+import episodeJSON from "./txfepisode.json" assert { type: "json" };
+console.log(episodeJSON);
 
 let btn = document.querySelector("#episode-button");
-btn.addEventListener("click", alertepisode);
+btn.addEventListener("click", AlertEpisode);
 
-function randomnum(min, max){
+function RandomNum(min, max){
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
 
-function seasonpicker() {
-    return randomnum(1, 11);
+function SeasonPicker() {
+    return RandomNum(1, 11);
 }
 
-function episodepicker(season) {
-    let selectedseasonobj = myjson[season];
-    let length = Object.keys(myjson[season]).length;
-    return randomnum(1, length);
+function EpisodePicker(season) {
+    let selSeasonObj = episodeJSON[season];
+    let length = Object.keys(selSeasonObj).length;
+    return RandomNum(1, length);
 }
 
-function getepisode(){
-    let season = seasonpicker();
-    let episode = episodepicker(season);
-    let seasonobj = myjson[season];
-    let epname = seasonobj[episode];
-    const epgen = [];
-    epgen[0] = `Season ${season} Episode ${episode}`;
-    epgen[1] = `"${epname}"`;
-    return epgen;
+function GetEpisode(){
+    let season = SeasonPicker();
+    let episode = EpisodePicker(season);
+    let seasonObj = myjson[season];
+    let SelEpisodeName = seasonObj[episode];
+    const episodeArray = [];
+    episodeArray[0] = `Season ${season} Episode ${episode}`;
+    episodeArray[1] = `"${SelEpisodeName}"`;
+    return episodeArray;
 }
 
-function alertepisode(){
-    let name = getepisode();
+function AlertEpisode(){
+    let name = GetEpisode();
     document.getElementById("episode-returned").innerHTML = `${name[0]} <br> ${name[1]}`;
 }
