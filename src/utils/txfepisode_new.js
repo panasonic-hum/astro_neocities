cd7c1d4b558bf56fd9b33ed55d79a1eb61bda094
shelby <thyredbasfasta@gmail.com>
2024-07-06 18:04:50 -0500

    txf generator filters

--- src/utils/txfepisode_new.js
+++ src/utils/txfepisode_new.js
@@ -1,16 +1,44 @@
 import txfJson from "./txfepisode_new.json" assert { type: "json" };
-console.log(txfJson);
 
-const mytharcObj = txfJson.map(
-  seasonRun.map((episodeRun) => console.log(episodeRun))
-);
-console.log(mytharcObj);
+window.onload = function () {
+  DisplayFilterMsg();
+};
 
+// button actions
 let btn = document.querySelector("#episode-button");
-btn.addEventListener("click", AlertEpisode);
+btn.addEventListener("click", () => AlertEpisode(0));
+let radioGrp = document.getElementById("filter-fieldset");
 
-let mytharcBtn = document.querySelector("#mytharc-button");
-btn.addEventListener("click", AlertEpisode);
+let filterCount = document.querySelector("#total-count");
+
+document.addEventListener("change", () => {
+  DisplayFilterMsg();
+});
+
+// functions
+function FilterByTag(tagIndex) {
+  const filterArray = txfJson.filter((episode) =>
+    episode.tags.includes(tagIndex)
+  );
+  return filterArray;
+}
+
+function DisplayFilterMsg() {
+  let tagIndex = GetFilterSelected();
+  let message;
+  switch (tagIndex) {
+    case 0:
+      message = "picking from all 217 episodes";
+      break;
+    case 1:
+      message = "picking from 71 mytharc episodes";
+      break;
+    case 2:
+      message = "picking from 146 monster of the week episodes";
+      break;
+  }
+  filterCount.innerHTML = `<i>${message}</i>`;
+}
 
 function RandomNum(min, max) {
   const minCeiled = Math.ceil(min);
@@ -18,30 +46,42 @@ function RandomNum(min, max) {
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
 
-function SeasonPicker() {
-  return RandomNum(1, 11);
+function GetFilteredEpisode(tagIndex) {
+  let filterArray = FilterByTag(tagIndex);
+  let arrayLength = filterArray.length;
+  let num = RandomNum(0, arrayLength - 1);
+  return filterArray[num];
 }
 
-function EpisodePicker(season) {
-  let selSeasonObj = txfJson[season];
-  let length = Object.keys(selSeasonObj).length;
-  return RandomNum(1, length);
+function GetAnyEpisode() {
+  let arrayLength = txfJson.length;
+  let num = RandomNum(0, arrayLength - 1);
+  return txfJson[num];
 }
 
-function GetEpisode() {
-  let season = SeasonPicker();
-  let episode = EpisodePicker(season);
-  let seasonObj = txfJson[season];
-  let SelEpisodeName = seasonObj[episode];
-  const episodeArray = [];
-  episodeArray[0] = `Season ${season} Episode ${episode}`;
-  episodeArray[1] = `"${SelEpisodeName}"`;
-  return episodeArray;
+function GetFilterSelected() {
+  if (document.getElementById("mytharc").checked) {
+    return 1;
+  } else if (document.getElementById("motw").checked) {
+    return 2;
+  } else return 0;
 }
 
 function AlertEpisode() {
-  let name = GetEpisode();
+  let filterIndex = GetFilterSelected();
+  let episode;
+  switch (filterIndex) {
+    case 1:
+      episode = GetFilteredEpisode(1);
+      break;
+    case 2:
+      episode = GetFilteredEpisode(2);
+      break;
+    default:
+      episode = GetAnyEpisode();
+  }
+  console.log(episode);
   document.getElementById(
     "episode-returned"
-  ).innerHTML = `${name[0]} <br> ${name[1]}`;
+  ).innerHTML = `${episode.episode_code} <br> ${episode.episode_name}`;
 }
